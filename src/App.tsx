7925341d905866a323542c867736436e91491f80
theghost1980 <saturnob612@gmail.com>
2023-03-18 16:09:28 -0400

    testing commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+//TODO
+//  - add router + navigation + router layout
+//  - add protected routes(user profile + user download history...etc)
+//  - add redux using toolkit for app state management
+//  - start redoing the whole site.
 const App = () => {
   return (
     <div>
         <h1>Hello React!</h1>
+        <div>I will be the new designed site for my dad!</div>
     </div>
   )
 }
