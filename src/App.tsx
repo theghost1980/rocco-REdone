39f224527b9a02dcda82a2dd22051870ea0705a9
theghost1980 <saturnob612@gmail.com>
2023-03-18 16:12:45 -0400

    updated: TODO list

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 //  - add protected routes(user profile + user download history...etc)
 //  - add redux using toolkit for app state management
 //  - start redoing the whole site.
+//  - install react bootstrap
+//  - find a better way to handle email subscriptions, image gallery & download files
+//  - would be interesting having a blog so Rocco may write, get responses, etc.
 const App = () => {
   return (
     <div>
